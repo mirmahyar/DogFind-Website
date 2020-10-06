480fdc00522c1cefd0ff3b9d4ebcaac59da70ea9
mirmahyar <m.mirsamadi@gmail.com>
2020-10-06 17:43:39 +0330

    enhancements

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -15,7 +15,7 @@ const Intro = () => {
                     <b>Welcome to the fascinating world of the dogs !
                     Here, you will learn many things about them that will help
                     you decide which brand is suitable for you !
-                    Selecting a dog can be a very hard decision to make, and here in dog land,
+                    Selecting a dog can be a very hard decision to make, and here,
                     we very well understand the challenge !
                     But we also realize that if you make this decision in an informed way, it will be the best decision of your
                     life!</b>
